38daca003f0549dc194deedbed0e400c6c522516
mainul35 <mainul.hasan@bs-23.net>
2019-12-16 02:08:10 +0600

    google map integration

--- src/main/java/com/brainstation/controller/HomeController.java
+++ src/main/java/com/brainstation/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class TodoController {
+public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index (Model model) {
         model.addAttribute("greetings", "Hello from Spring");
